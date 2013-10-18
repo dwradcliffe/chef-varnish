de018fca9116531d6b37ce48e0f916ceb10270a9
David Radcliffe <radcliffe.david@gmail.com>
2013-10-18 17:08:12 -0400

    use epel recipe from yum cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,13 @@
+name             "varnish"
 maintainer       "David Radcliffe"
 maintainer_email "radcliffe.david@gmail.com"
 license          "MIT"
 description      "Installs/Configures varnish server"
 
-version          "0.0.1"
+version          "0.0.2"
+
+recipe           "varnish::default",  "NOOP"
+recipe           "varnish::server",   "Install varnish."
+recipe           "varnish::yum_repo", "Install varnish yum repo."
+
+depends          "yum"
--- recipes/server.rb
+++ recipes/server.rb
@@ -8,7 +8,7 @@
 case node['platform_family']
 when "rhel", "fedora"
   major_varnish_version = node['varnish']['version'].split(".")[0]
-  include_recipe "epel" if major_varnish_version == "2"
+  include_recipe "yum::epel" if major_varnish_version == "2"
   include_recipe "varnish::yum_repo" if major_varnish_version == "3"
 end
 
